47dfe6b642e1ce109be55a281629ab17b28feda5
jwietrzykowski <jan.m.wietrzykowski@gmail.com>
2020-01-15 16:02:17 +0100

    Added code for point cloud visualization.

--- main.py
+++ main.py
@@ -1,138 +1,159 @@
 import numpy as np
 import tensorflow as tf
-import cv2 as cv
+import cv2
 import os
+import open3d
+import argparse
+import fnmatch
 
 
-def read_calib_file(filepath):
-    """Read in a calibration file and parse into a dictionary."""
-    data = {}
+def scale_and_crop(im, h, w, K):
+    im_h = im.shape[0]
+    im_w = im.shape[1]
+    scale = w / im_w
 
-    with open(filepath, 'r') as f:
-        for line in f.readlines():
-            key, value = line.split(':', 1)
-            # The only non-float values in these files are dates, which
-            # we don't care about anyway
-            try:
-                data[key] = np.array([float(x) for x in value.split()])
-            except ValueError:
-                pass
+    im = cv2.resize(im, dsize=(0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+    K[0, 0] *= scale
+    K[1, 1] *= scale
 
-    return data
+    im_h = im.shape[0]
+    im_w = im.shape[1]
 
+    off_x = (im_w - w) // 2
+    off_y = (im_h - h) // 2
+    im = im[off_y: off_y + h, off_x: off_x + w]
+    K[0, 2] -= off_x
+    K[1, 2] -= off_y
 
-def load_cam_to_cam(filepath):
-    data = {}
-
-    filedata = read_calib_file(filepath)
-
-    P_rect_20 = np.reshape(filedata['P_rect_02'], (3, 4))
-    P_rect_30 = np.reshape(filedata['P_rect_03'], (3, 4))
-
-    R_rect_20 = np.eye(4)
-    R_rect_20[0:3, 0:3] = np.reshape(filedata['R_rect_02'], (3, 3))
-    R_rect_30 = np.eye(4)
-    R_rect_30[0:3, 0:3] = np.reshape(filedata['R_rect_03'], (3, 3))
-
-    T2 = np.eye(4)
-    T2[0, 3] = P_rect_20[0, 3] / P_rect_20[0, 0]
-    T3 = np.eye(4)
-    T3[0, 3] = P_rect_30[0, 3] / P_rect_30[0, 0]
-
-    p_cam = np.array([0, 0, 0, 1])
-    p_2 = np.linalg.inv(T2).dot(p_cam)
-    p_3 = np.linalg.inv(T3).dot(p_cam)
-
-    data['K_cam2'] = P_rect_20[0:3, 0:3]
-    data['K_cam3'] = P_rect_30[0:3, 0:3]
-
-    data['baseline'] = np.linalg.norm(p_3 - p_2)
-
-    data['imWidth'] = filedata['S_rect_02'][0]
-    data['imHeight'] = filedata['S_rect_02'][1]
-
-    return data
+    return im, K
 
 
-depthLim = 65535
+def main():
+    parser = argparse.ArgumentParser(description='Estimate depth from stereo images')
+    parser.add_argument('inputdir',
+                        help='input directory')
+    parser.add_argument('outputdir',
+                        help='output directory')
+    parser.add_argument('-v', '--verbose', action="store_true", default=False,
+                        help='verbose output')
 
-# paths
-datasetPath = "/media/jachu/JanW/KITTI_2015_stereo/data_scene_flow/testing"
-calibPath = "/media/jachu/JanW/KITTI_2015_stereo/data_scene_flow_calib/testing/calib_cam_to_cam/000000.txt"
+    args = parser.parse_args()
 
-# calibration parameters
-calibData = load_cam_to_cam(calibPath)
+    # depthLim = 65535
+    depthLim = 20000
 
-print('K_cam2', calibData['K_cam2'])
-print('K_cam3', calibData['K_cam3'])
-print('baseline', calibData['baseline'])
-print('imWidth', calibData['imWidth'])
-print('imHeight', calibData['imHeight'])
+    left_list = sorted(fnmatch.filter(os.listdir(os.path.join(args.inputdir, 'left')), '*.png'))
+    right_list = sorted(fnmatch.filter(os.listdir(os.path.join(args.inputdir, 'right')), '*.png'))
 
-imWidth = calibData['imWidth']
-fx = calibData['K_cam2'][0, 0]
-baseline = calibData['baseline']
+    if args.verbose:
+        print('Number of left images: %d' % len(left_list))
+        print('Number of right images: %d' % len(right_list))
 
-# path to .meta
-# loader = tf.train.import_meta_graph('data/model-inference-1025x321-0.meta')
-loader = tf.train.import_meta_graph('data/model-inference-513x257-0.meta')
+    # calibration parameters
+    camera_file = os.path.join(args.inputdir, 'camera.txt')
+    cam_params = np.fromfile(camera_file, dtype=np.float, sep=' ')
+    K = np.eye(3, dtype=np.float)
+    # fx
+    K[0, 0] = cam_params[0]
+    # fy
+    K[1, 1] = cam_params[1]
+    # cx
+    K[0, 2] = cam_params[2]
+    # cy
+    K[1, 2] = cam_params[3]
 
-# filename as input
-input_img_1 = tf.get_default_graph().get_tensor_by_name("Dataloader/StringJoin:0")
-input_img_2 = tf.get_default_graph().get_tensor_by_name("Dataloader/StringJoin_1:0")
-disp_left = tf.get_default_graph().get_tensor_by_name("disparities/ExpandDims:0")
+    # im_width = cam_params[4]
+    # im_height = cam_params[5]
 
-config = tf.ConfigProto(allow_soft_placement=True, inter_op_parallelism_threads=2, intra_op_parallelism_threads=1)
-with tf.Session(config=config) as sess:
-    # restore model parameters
-    loader.restore(sess, 'data/model-inference-513x257-0')
+    baseline = cam_params[6]
 
-    # for graph inspection in tensorboard
-    train_writer = tf.summary.FileWriter('summary', sess.graph)
-    # train_writer.flush()
+    # path to .meta
+    # loader = tf.train.import_meta_graph('data/model-inference-1025x321-0.meta')
+    loader = tf.train.import_meta_graph('data/model-inference-513x257-0.meta')
+    network_im_width = 513
+    network_im_height = 257
 
-    # print out all variables
-    # names = [n.name for n in tf.get_default_graph().as_graph_def().node]
-    #for name in names:
-    #    print(name)
+    # filename as input
+    input_img_1_tensor = tf.get_default_graph().get_tensor_by_name('Dataloader/read_image/read_png_image/DecodePng:0')
+    input_img_2_tensor = tf.get_default_graph().get_tensor_by_name('Dataloader/read_image_1/read_png_image/DecodePng:0')
+    disp_left = tf.get_default_graph().get_tensor_by_name("disparities/ExpandDims:0")
 
-    leftImFileList = sorted(os.listdir(datasetPath + "/image_2"))
-    rightImFileList = sorted(os.listdir(datasetPath + "/image_3"))
+    config = tf.ConfigProto(allow_soft_placement=True, inter_op_parallelism_threads=2, intra_op_parallelism_threads=1)
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        # restore model parameters
+        # loader.restore(sess, 'data/model-inference-1025x321-0')
+        loader.restore(sess, 'data/model-inference-513x257-0')
 
-    for i, entry in enumerate(leftImFileList):
-        print('leftIm: ', leftImFileList[i])
-        print('rightIm: ', rightImFileList[i])
-
-        # run
-        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-        run_metadata = tf.RunMetadata()
-        merged = tf.summary.merge_all()
-        summary, disp = sess.run([merged, disp_left],
-                                 feed_dict={input_img_1: datasetPath + "/image_2/" + leftImFileList[i],
-                                            input_img_2: datasetPath + "/image_3/" + rightImFileList[i]},
-                                 options=run_options,
-                                 run_metadata=run_metadata)
-        train_writer.add_run_metadata(run_metadata, 'run%d' % i, i)
-        train_writer.add_summary(summary, i)
+        # for graph inspection in tensorboard
+        train_writer = tf.summary.FileWriter('summary', sess.graph)
         # train_writer.flush()
 
-        print('output', disp.shape)
+        # print out all variables
+        # names = [n.name for n in tf.get_default_graph().as_graph_def().node]
+        #for name in names:
+        #    print(name)
 
-        # select a slice from first dimension
-        disp = disp[0]
+        for i, entry in enumerate(left_list):
+            print('leftIm: ', left_list[i])
+            print('rightIm: ', right_list[i])
 
-        print('min disp = ', np.min(disp))
-        print('max disp = ', np.max(disp))
+            left_im = cv2.imread(os.path.join(args.inputdir, 'left', left_list[i]))
+            right_im = cv2.imread(os.path.join(args.inputdir, 'right', right_list[i]))
 
-        # depth in [mm]
-        depth = np.uint16(1000 * baseline * fx / (disp * imWidth))
-        dispLim = 1000 * baseline * fx / (depthLim * imWidth)
-        print('dispLim ', dispLim)
-        depth[disp < dispLim] = 0
+            [left_im, K_cur] = scale_and_crop(left_im, network_im_height, network_im_width, K)
+            [right_im, K_cur] = scale_and_crop(right_im, network_im_height, network_im_width, K)
 
-        # save depth image
-        cv.imwrite('out_depth/depth_' + leftImFileList[i], depth)
+            fx = K_cur[0, 0]
 
-        # display image
-        cv.imshow("out", depth)
-        cv.waitKey(-1)
+            # run
+            run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            run_metadata = tf.RunMetadata()
+            merged = tf.summary.merge_all()
+            summary, disp = sess.run([merged, disp_left],
+                                     feed_dict={input_img_1_tensor: left_im,
+                                                input_img_2_tensor: right_im},
+                                     options=run_options,
+                                     run_metadata=run_metadata)
+            train_writer.add_run_metadata(run_metadata, 'run%d' % i, i)
+            train_writer.add_summary(summary, i)
+            # train_writer.flush()
+
+            print('output', disp.shape)
+
+            # select a slice from first dimension
+            disp = disp[0]
+
+            print('min disp = ', np.min(disp))
+            print('max disp = ', np.max(disp))
+
+            # depth in [mm]
+            depth = baseline * fx / (disp * network_im_width)
+            depthUint = np.uint16(1000 * depth)
+            dispLim = 1000 * baseline * fx / (depthLim * network_im_width)
+            print('dispLim ', dispLim)
+            depth[disp < dispLim] = 0
+            depthUint[disp < dispLim] = 0
+
+            # save depth image
+            cv2.imwrite(os.path.join(args.outputdir, left_list[i].replace('.jpg', '_depth.png')), depthUint)
+
+            # display image
+            cv2.imshow("left", left_im)
+            cv2.imshow("right", right_im)
+            cv2.imshow("depth", depthUint)
+            cv2.waitKey(100)
+
+            points3d = cv2.rgbd.depthTo3d(depth, K_cur)
+
+            # visualization of point cloud for testing purposes
+            pointcloud = open3d.geometry.PointCloud()
+            pointcloud.points = open3d.Vector3dVector(np.reshape(points3d, [-1, 3]))
+            pointcloud.colors = open3d.Vector3dVector(np.reshape(left_im.astype(np.float32)/255.0, [-1, 3]))
+            open3d.draw_geometries([pointcloud])
+
+            cv2.waitKey(-1)
+
+
+if __name__ == "__main__":
+    main()
